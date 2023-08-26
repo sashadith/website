9623e02f7c38df411983f8e28f888d6c16f247c4
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-08-26 03:15:14 +0200

    edit first screen

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,28 +1,44 @@
 'use client'
-import React from 'react'
-import Image from 'next/image'
-import { motion } from 'framer-motion'
+import React, { useEffect, useRef } from 'react';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { gsap } from 'gsap';
 
-import style from './Hero.module.scss'
+import style from './Hero.module.scss';
 
-import LogoImage from '../../assets/images/logo.png'
+import HeroImage from '../../assets/images/hero.jpg';
+import LogoImage from '../../assets/images/logo.png';
 
 export const Hero = () => {
+  const logoRef = useRef(null);
+
+  useEffect(() => {
+    const logoTimeline = gsap.timeline({ repeat: 0 });
+
+    // Первый короткий скретч
+    logoTimeline.to(logoRef.current, { rotation: -360, duration: 0.8, ease: 'slow(0.7, 0.7, false)' });
+
+    // Долгая анимация на 360 градусов (эффект пластинки)
+    logoTimeline.to(logoRef.current, { rotation: '+=330', duration: 2, ease: 'none' }, '-=0.2');
+
+  }, []);
+
   return (
     <section className={style.hero}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        className={style.heroLogo}
+        ref={logoRef}
+      >
+        <Image src={LogoImage} alt="Sasha Dith Logo" />
+      </motion.div>
       <div className="container">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          className={style.heroLogo}
-        >
-          <Image
-            src={LogoImage}
-            alt="Sasha Dith Logo"
-          />
-        </motion.div>
+        <div className={style.backgroundImage}>
+          <Image src={HeroImage} alt="Sasha Dith" className={style.heroImage} />
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
