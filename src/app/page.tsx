dedf0a82ee94997923ddfb3541871b12bdab2f60
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-08-28 11:32:09 +0200

    edit parallax and animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { Hero } from '../components/Hero/Hero';
-import { Reserve } from '../components/Reserve/Reserve';
 
 export default function Home() {
   
   return (
     <main>
       <Hero />
-      <Reserve />
     </main>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,58 +1,21 @@
 'use client'
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import style from './Hero.module.scss';
 
 import HeroImage from '../../assets/images/hero.jpg';
 import LogoImage from '../../assets/images/SashaDithLogo.png';
-import AboutImage from '../../assets/images/aboutImage.jpg';
-
-const phrase = "Sasha Dith — DJ and producer born in the USSR, grew up in Germany, entered the music business in 2005 with his debut single \"RUSSIAN GIRLS,\" which reached the top of the charts in many countries. The \"RUSSIAN GIRLS\" video, showcasing the power and beauty of Russian women, got more than 40,000,000 views within a few weeks on YouTube, Metacafe & Co., and rose to become one of the most viewed music videos on the internet ever! TV shows and club gigs have taken Sasha around the world. He continues to produce music and bring joy to his fans with his DJ sets.";
+import AboutImage from '../../assets/images/aboutImage1.jpg';
 
 export const Hero = () => {
 
-  let refs = useRef([]);
-  const container = useRef(null);
-
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    createAnimation();
-  }, [])
-
-  const createAnimation = () => {
-    gsap.to(refs.current, {
-      scrollTrigger: {
-        trigger: container.current,
-        scrub: true,
-        start: `top top+=350`,
-        end: `+=${window.innerHeight / 2.3}`,
-      },
-      opacity: 1,
-      ease: "none",
-      stagger: 0.1
-    })
-  }
-
-  const splitWords = (phrase) => {
-    let body = [];
-    phrase.split(" ").forEach((word, i) => {
-      const letters = splitLetters(word);
-      body.push(<p key={word + "_" + i}>{letters}</p>)
-    })
-    return body
-  }
-
-  const splitLetters = (word) => {
-    let letters = []
-    word.split("").forEach((letter, i) => {
-      letters.push(<span key={letter + "_" + i} ref={el => { refs.current.push(el) }}>{letter}</span>)
-    })
-    return letters;
-  }
+    AOS.init();
+  }, []);
 
   return (
     <>
@@ -79,7 +42,7 @@ export const Hero = () => {
         </div>
       </section>
       <div className={style.parallax}></div>
-      <section ref={container} className={style.about}>
+      <section className={style.about}>
         <div className="container">
           <div className={style.aboutBlock}>
             <div className={style.aboutBlockImage}>
@@ -91,11 +54,9 @@ export const Hero = () => {
             </div>
             <div className={style.aboutBlockContent}>
               <h2 className={style.aboutTitle}>About</h2>
-              <div className={style.aboutText}>
-                {
-                  splitWords(phrase)
-                }
-              </div>
+              <p data-aos="fade-up" className={style.aboutText}>
+                Sasha Dith — DJ and producer born in the USSR, grew up in Germany, entered the music business in 2005 with his debut single "RUSSIAN GIRLS," which reached the top of the charts in many countries. The "RUSSIAN GIRLS" video, showcasing the power and beauty of Russian women, got more than 40,000,000 views within a few weeks on YouTube, Metacafe & Co., and rose to become one of the most viewed music videos on the internet ever! TV shows and club gigs have taken Sasha around the world. He continues to produce music and bring joy to his fans with his DJ sets.
+              </p>
             </div>
           </div>
         </div>
