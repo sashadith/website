f7b3bc3cd5b31a0961abe5702f2665289f730250
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-11-03 20:14:27 +0100

    ed ga code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,31 @@ import { Hero } from '../components/Hero/Hero';
 import { Videos } from '../components/Videos/Videos';
 import { Music } from '../components/Music/Music';
 import { Contact } from '../components/Contact/Contact';
-// import { Scrolling } from '../components/Scrolling/Scrolling';
+import Script from 'next/script';
 
 export default function Home() {
   
   return (
-    <main>
-      <Hero />
-      <Videos />
-      <Music />
-      <Contact />
-      {/* <Scrolling /> */}
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2XCZ4C7BCY"
+      />
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-2XCZ4C7BCY');
+        `}
+      </Script>
+      <main>
+        <Hero />
+        <Videos />
+        <Music />
+        <Contact />
     </main>
+    </>
   )
 }
