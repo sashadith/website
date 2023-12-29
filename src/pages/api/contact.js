e3566cdfc5e1e1ab91ddb5da200e5bfa0aad5645
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-12-29 12:56:59 +0100

    add pages

---
+++ src/pages/api/contact.js
@@ -0,0 +1,39 @@
+// pages/api/contact.js
+import nodemailer from 'nodemailer';
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const { name, phone, email, subject, message } = req.body;
+
+      // Создаем транспорт для отправки почты (замените данными вашего почтового сервера)
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com', // Хост для Gmail SMTP
+        port: 465, // Порт для SSL
+        secure: true, // Использовать SSL
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+
+      // Опции для отправки письма
+      const mailOptions = {
+        from: email,
+        to: process.env.EMAIL_USER,
+        subject: `New message from ${name} - ${subject}`,
+        text: `${message}\n\nContact Details:\nName: ${name}\nPhone: ${phone}\nEmail: ${email}`,
+      };
+
+      // Отправляем письмо
+      await transporter.sendMail(mailOptions);
+
+      res.status(200).json({ message: 'Message sent successfully!' });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ message: 'Error sending email' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
